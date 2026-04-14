80943d459ccce43568fc2a8a6d638408109f1f0d
Michael Trullas Garcia <adoser2@hotmail.es>
2026-04-14 21:42:46 +0200

    stryker

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -1,18 +1,27 @@
+// /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+// const config = {
+//   packageManager: 'npm',
+//   reporters: ['html', 'clear-text', 'progress'],
+//   testRunner: 'command',
+//   coverageAnalysis: 'off',
+//   buildCommand: 'node start-server.js',
+//   mutate: ['src/app/features/customers/customer.service.ts'],
+//   //mutate: ['src/app/services/title.service.ts', 'src/app/services/customer.service.ts'],
+//   //mutate: ['src/app/**/*.ts'],
+//   commandRunner: {
+//     command: 'node run-playwright.js'
+//   },
+//   logLevel: 'trace',
+//   fileLogLevel: 'trace',
+//   timeoutFactor: 3
+// };
+// export default config;
+
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 const config = {
-  packageManager: 'npm',
-  reporters: ['html', 'clear-text', 'progress'],
-  testRunner: 'command',
-  coverageAnalysis: 'off',
-  buildCommand: 'node start-server.js',
-  mutate: ['src/app/features/customers/customer.service.ts'],
-  //mutate: ['src/app/services/title.service.ts', 'src/app/services/customer.service.ts'],
-  //mutate: ['src/app/**/*.ts'],
-  commandRunner: {
-    command: 'node run-playwright.js'
-  },
-  logLevel: 'trace',
-  fileLogLevel: 'trace',
-  timeoutFactor: 3
+  "testRunner": "vitest",
+  "vitest": {
+    "related": false
+  }
 };
 export default config;
