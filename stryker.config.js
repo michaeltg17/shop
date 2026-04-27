5bbc4d6730daeb8ddc31fa43873f172334faf1e4
Michael Trullas Garcia <adoser2@hotmail.es>
2026-04-27 02:32:12 +0200

    wip

--- stryker.config.js
+++ stryker.config.js
@@ -9,12 +9,19 @@ module.exports = {
     projectType: 'custom',
     configFile: 'jest.config.ts'
   },
-  reporters: ['html', 'clear-text', 'progress'],
+  reporters: ['html', 'clear-text', 'progress', 'dashboard'],
   coverageAnalysis: 'perTest',
   tsconfigFile: 'tsconfig.json',
   cleanTempDir: 'always',
   incremental: true,
   thresholds: {
     break: 82
+  },
+  dashboard: {
+    project: 'github.com/michaeltg17/Angular',
+    module: 'angular-app',
+    version: process.env.GITHUB_SHA || process.env.BRANCH_NAME || 'local',
+    baseUrl: 'https://dashboard.stryker-mutator.io',
+    reportType: 'mutation'
   }
 };
