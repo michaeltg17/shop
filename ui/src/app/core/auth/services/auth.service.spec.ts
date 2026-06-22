7211fe1e69ff093e4342ebb726c4b70261e5dfad
skynet-statikk <statikk.worker@outlook.com>
2026-06-22 04:57:50 +0200

    style: remove unused imports from auth.service.spec.ts (HttpClient, of, Observable)

--- ui/src/app/core/auth/services/auth.service.spec.ts
+++ ui/src/app/core/auth/services/auth.service.spec.ts
@@ -1,8 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthService, User } from './auth.service';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
-import { of } from 'rxjs';
 
 describe('AuthService', () => {
   let service: AuthService;
